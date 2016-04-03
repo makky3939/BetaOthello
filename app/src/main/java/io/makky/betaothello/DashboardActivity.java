3b96cbef2a2004b9e2fa1be83d34f75a803cd040
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-04-03 15:37:36 +0900

    add move to board activity action

--- app/src/main/java/io/makky/betaothello/DashboardActivity.java
+++ app/src/main/java/io/makky/betaothello/DashboardActivity.java
@@ -1,7 +1,10 @@
 package io.makky.betaothello;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button move2board = (Button) findViewById(R.id.move2board);
+
+        move2board.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, BoardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }

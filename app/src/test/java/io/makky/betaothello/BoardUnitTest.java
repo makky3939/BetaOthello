6eb7d1592998e79dcde7d01c41455fea84188ed2
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-05-04 11:59:53 +0900

    update game board ui

--- app/src/test/java/io/makky/betaothello/BoardUnitTest.java
+++ app/src/test/java/io/makky/betaothello/BoardUnitTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class BoardUnitTest {
     @Test
     public void initTest() throws Exception {
-        Board board = new Board();
+        GameBoard board = new GameBoard ();
 
-        assertEquals(8, board.getState().length);
+        assertEquals(8, board.getBoard().length);
     }
 }
